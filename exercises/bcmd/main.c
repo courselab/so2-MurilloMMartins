2cf54bc4d413c0312cdcbf07bb8b7ffc5da6078b
MurilloMMartins <murillomartins19@gmail.com>
2024-05-08 18:08:37 -0300

    Developed a new function called drawSmiley

--- exercises/bcmd/main.c
+++ exercises/bcmd/main.c
@@ -17,25 +17,34 @@
 
 char buffer[SIZE];		/* Read buffer.      */
 
+void drawSmiley() {
+  readln(buffer);
+  short Xlocation = buffer[0] - 48;
+
+  readln(buffer);
+  short Ylocation = buffer[0] - 48;
+
+  short *video = (short *) 0xb8000 + 80*Ylocation + Xlocation;
+  video[0] = (Xlocation*Ylocation+1 << 8) + 2;
+}
+
 int main()
 {
   clear();
   
   println  ("Boot Command 1.0");
 
-  while (1)
-    {
+  while (1) {
       print(PROMPT);		/* Show prompt.               */
       readln(buffer);		/* Read use input.            */
 
-      if (buffer[0])		/* Execute built-in command.  */
-	{
-	  if (!strcmp(buffer,"help"))
-	    println("A Beattles's song.");
-	  else 
-	    println("Unkown command.");
-	}
-    }
+      if (buffer[0]) {	/* Execute built-in command.  */
+        if (!strcmp(buffer,"smiley"))
+          drawSmiley();
+        else 
+          println("Unkown command.");
+	  }
+  }
   
   return 0;
 
